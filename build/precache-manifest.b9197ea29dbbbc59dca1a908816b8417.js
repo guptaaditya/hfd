50fa601b04aa4898d21f869c342df592cbe03034
Aditya Gupta <guptaaditya24@gmail.com>
2020-08-27 22:36:56 +0530

    build deploy

---
+++ build/precache-manifest.b9197ea29dbbbc59dca1a908816b8417.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5051f1d0abb417a66d41e15e3b070b4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ed9efb1107c1e439703",
+    "url": "/static/css/main.3f3de4c4.chunk.css"
+  },
+  {
+    "revision": "c1d62556d76743ff299e",
+    "url": "/static/js/2.7cf285cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7cf285cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ed9efb1107c1e439703",
+    "url": "/static/js/main.feb91bf1.chunk.js"
+  },
+  {
+    "revision": "5f90baee4ce359b4ed1f",
+    "url": "/static/js/runtime-main.2cbd8d11.js"
+  }
+]);
